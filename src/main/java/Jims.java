672bde6868a402d52ed45d3d20f1f5d6fea346b5
Martin Goberg <martingoberg@gmail.com>
2024-02-25 16:10:50 +0100

    Code Cleanup
    Added connDB Method to shorten code and to prevent repetition of code.
    
    Cleaned up and commented

--- src/main/java/Jims.java
+++ src/main/java/Jims.java
@@ -21,15 +21,23 @@ import static java.awt.AWTEventMulticaster.add;
 public class Jims {
     static MongoClient client;
     static MongoCollection<Document> usersCollection;
-    private static DefaultTableModel table = new DefaultTableModel();
+    private static final DefaultTableModel table = new DefaultTableModel();
     private static final String[] columnNames = {"Name", "Price", "Description", "Quantity"};
-    private JTable table1;
+    static DefaultTableModel model = new DefaultTableModel(columnNames, 0);
 
+    static MongoCollection<Document> collection;
 
+    // Establish Database connection
+    public static void connDB() {
+        client = MongoClients.create("mongodb+srv://martingoberg:root@jims.byniw4p.mongodb.net/?retryWrites=true&w=majority");
+        MongoDatabase database = client.getDatabase("items");
+        MongoCollection<Document> collection = database.getCollection("items");
+    }
+
+    // Load users table
     public static void loadUsers() {
         try {
-            client = MongoClients.create("mongodb+srv://martingoberg:root@jims.byniw4p.mongodb.net/?retryWrites=true&w=majority");
-            System.out.println("MongoDB connected! Ignore error for logging.");
+            connDB();
             MongoDatabase database = client.getDatabase("users");
             usersCollection = database.getCollection("users");
         } catch (Exception e) {
@@ -37,12 +45,12 @@ public class Jims {
         }
     }
 
+    // Load items table
     public static List<Document> loadItems() {
         List<Document> documents = new ArrayList<>();
 
         try {
-            MongoClient client = MongoClients.create("mongodb+srv://martingoberg:root@jims.byniw4p.mongodb.net/?retryWrites=true&w=majority");
-            System.out.println("Loaded table: Items");
+            connDB();
             MongoDatabase database = client.getDatabase("items");
             MongoCollection<Document> collection = database.getCollection("items");
 
@@ -54,7 +62,7 @@ public class Jims {
 
         return documents;
     }
-
+    // Get tableModel
     public static DefaultTableModel getTableModel() {
         List<Document> documents = loadItems();
         String[] columnNames = {"Name", "Price", "Description", "Quantity"};
@@ -67,7 +75,6 @@ public class Jims {
 
         return model;
     }
-
 
     // Authenticate user
     public static boolean authenticateUser(String username, String password) {
@@ -100,7 +107,7 @@ public class Jims {
     }
 
     public static void insertItemIntoMongoDB(String itemName, double price, String desc, int qty) {
-        MongoClient client = MongoClients.create("mongodb+srv://martingoberg:root@jims.byniw4p.mongodb.net/?retryWrites=true&w=majority");
+        connDB();
         MongoDatabase database = client.getDatabase("items");
         MongoCollection<Document> collection = database.getCollection("items");
 
@@ -115,63 +122,23 @@ public class Jims {
 
         System.out.println("Item inserted successfully!");
     }
-    public static void updateJTable(JTable table) {
-        table.repaint();
-        System.out.println("Debugging!!! UpdateJTable called");
 
-    }
-
-    static void displayDatabase() {
-        MongoClient client = MongoClients.create("mongodb+srv://martingoberg:root@jims.byniw4p.mongodb.net/?retryWrites=true&w=majority");
-        System.out.println("Connected displayDatabase();");
-        MongoDatabase database = client.getDatabase("items");
-        MongoCollection<Document> collection = database.getCollection("items");
-
-        List<Document> documents = collection.find().into(new ArrayList<>());
-
-        String[] columnNames = {"Name", "Price", "Description", "Quantity"};
-        DefaultTableModel model = new DefaultTableModel(columnNames, 0);
-
-        for (Document document : documents) {
-            Object[] rowData = {document.get("name"), document.get("price"), document.get("desc"), document.get("qty")};
-            model.addRow(rowData);
-        }
-
-        System.out.println("Data from the database:");
-        for (int i = 0; i < model.getRowCount(); i++) {
-            for (int j = 0; j < model.getColumnCount(); j++) {
-                System.out.print(model.getValueAt(i, j) + "\t");
-            }
-            System.out.println();
-        }
-    }
-
+    // Retrieves the table
     public static JTable getTable() {
-        MongoClient client = MongoClients.create("mongodb+srv://martingoberg:root@jims.byniw4p.mongodb.net/?retryWrites=true&w=majority");
-        System.out.println("Connected to Items > Items in MongoDB");
-        MongoDatabase database = client.getDatabase("items");
-        MongoCollection<Document> collection = database.getCollection("items");
-
-        List<Document> documents = collection.find().into(new ArrayList<>());
-
-        String[] columnNames = {"Name", "Price", "Description", "Quantity"};
-        DefaultTableModel model = new DefaultTableModel(columnNames, 0);
-
-        for (Document document : documents) {
-            Object[] rowData = {document.get("name"), document.get("price"), document.get("desc"), document.get("qty")};
-            model.addRow(rowData);
-        }
-
         JTable table = new JTable(model);
+        DefaultTableModel updatedModel = Jims.getTableModel();
+        table.setModel(updatedModel);
+        table.repaint();
         table.setLayout(new BorderLayout());
         table.setBounds(600, 95, 570, 700);
 
         return table;
     }
 
+    // Delete item from table / database
     public static void deleteItemFromMongoDB(int rowIndex) {
         try {
-            MongoClient client = MongoClients.create("mongodb+srv://martingoberg:root@jims.byniw4p.mongodb.net/?retryWrites=true&w=majority");
+            connDB();
             MongoDatabase database = client.getDatabase("items");
             MongoCollection<Document> collection = database.getCollection("items");
 
@@ -190,10 +157,4 @@ public class Jims {
             e.printStackTrace();
         }
     }
-
 }
-
-
-
-
-
